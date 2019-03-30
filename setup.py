c6d32fcd6f61bc6a33c725d0f4339e261950129e
Charles Demontigny <charles@charlesdemontigny.com>
2019-03-30 08:59:00 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A package to analyze messenger conversation and create insight from it.',
+    author='Charles Demontigny',
+    license='MIT',
+)
