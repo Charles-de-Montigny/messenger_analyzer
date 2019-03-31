bafd67b71b25ceda115baf05f8ca177ca327f824
Charles Demontigny <charles@charlesdemontigny.com>
2019-03-31 10:49:52 -0400

    able to load JSON into pandas DataFrame

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,39 @@
 # -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+#!/usr/bin/env python3
 
+import json
+import pandas as pd
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+def make_dataset(input_path):
+    """Load JSON file and transform it into pandas DataFrames.
+    
+    Args:
+        input_path: str
+            The path to the JSON file.
+    
+    Returns:
+        Messages pandas DataFrame
+        Participants pandas DataFrame
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
+    # Load JSON file
+    with open(input_path) as json_data:
+        d = json.load(json_data)
+    # Messages
+    tmp = []
+    for message in d['messages']:
+        tmp.append(message)
+    messages = pd.DataFrame(tmp)
+    messages['time'] = pd.to_datetime(messages['timestamp_ms'], unit = 'ms')
+    messages['date'] = pd.to_datetime(messages['timestamp_ms'], unit = 'ms').dt.strftime('%Y-%m-%d')
+    messages['month'] = pd.to_datetime(messages['timestamp_ms'], unit = 'ms').dt.strftime('%Y-%m')
+    messages['id'] = messages.index
+    # Participants
+    tmp = []
+    for participant in d['participants']:
+        tmp.append(participant)
+    participants = pd.DataFrame(tmp)
+    return messages, participants
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    messages, participants = make_dataset("data/raw/sundayrose/message_1.json")
+    import pdb; pdb.set_trace()
