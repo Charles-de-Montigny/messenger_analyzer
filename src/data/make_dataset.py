0e468efa0a2d92ef8d92f7b19555bc446fb6306e
Charles Demontigny <charles@charlesdemontigny.com>
2019-04-01 09:32:26 -0400

    make_messages() make_participants() works

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,6 +4,87 @@
 import json
 import pandas as pd
 
+def extract_contents(df, content_col):
+    """Extract not null contents from the columns, rename the content columns
+    'content' and creates a content_type column.
+    
+    Args:
+        df: pandas DataFrame
+            The DataFrame from which the content is extracted.
+        content_col: str
+            The column from which to extract the content from.
+    
+    Returns: A pandas DataFrame with the id column and the content column.
+    """
+    content_df = df.loc[pd.notnull(df[content_col]), [content_col, 'id']]
+    content_df.rename(columns={content_col:'content'}, inplace = True)
+    content_df['content_type'] = content_col
+    return content_df
+
+def make_messages(dict):
+    """Returns clean message DataFrame from the dictionnary.
+    
+    Args:
+        dict: python dictionnary
+
+    Returns: A pandas DataFrame with content, id, content_type, sender_name and time.
+    """
+    # Unpackaed messages from dict
+    tmp = []
+    for message in dict['messages']:
+        tmp.append(message)
+    messages = pd.DataFrame(tmp)
+    messages['time'] = pd.to_datetime(messages['timestamp_ms'], unit = 'ms')
+    messages['id'] = messages.index
+    messages = messages.query('type == "Generic"')
+
+    # Reshape DataFrame into a single content columns
+    content = messages.loc[pd.notnull(messages['content']), ['content', 'id']]
+    content['content_type'] = 'messages'
+
+    contents = [content]
+    content_cols = ['audio_files', 'files', 'gifs', 'photos', 'share', 'sticker', 'videos']
+    for col in content_cols:
+        contents.append(extract_contents(messages, col))
+    contents_df = pd.concat(contents).sort_values('id')
+    messages_df = pd.merge(contents_df, messages[['sender_name', 'time', 'id']], on = 'id')
+    return messages_df
+
+def make_participants(dict):
+    """Returns a DataFrame with the participants.
+    
+    Args:
+        dict: python dictionnary
+
+    Returns: A pandas DataFrame
+    """
+    tmp = []
+    for participant in dict['participants']:
+        tmp.append(participant)
+    return pd.DataFrame(tmp)
+
+def make_reactions(dict):
+    """Returns a DataFrame with the reactions.
+    
+    Args:
+        dict: python dictionnary
+
+    Returns: A pandas DataFrame
+    """
+    # Unpackaed messages from dict
+    tmp = []
+    for message in dict['messages']:
+        tmp.append(message)
+    messages = pd.DataFrame(tmp)
+    reactions_dict = messages.loc[:, ['reactions']]
+    reactions_dict.dropna(inplace = True)
+    reactions_df = pd.DataFrame()
+    for idx, row in reactions_dict.iterrows():
+        tmp = pd.DataFrame(row['reactions'])
+        tmp['id'] = idx
+        reactions_df = reactions_df.append(tmp)
+    return reactions_df
+
 def make_dataset(input_path):
     """Load JSON file and transform it into pandas DataFrames.
     
@@ -19,20 +100,13 @@ def make_dataset(input_path):
     with open(input_path) as json_data:
         d = json.load(json_data)
     # Messages
-    tmp = []
-    for message in d['messages']:
-        tmp.append(message)
-    messages = pd.DataFrame(tmp)
-    messages['time'] = pd.to_datetime(messages['timestamp_ms'], unit = 'ms')
-    messages['date'] = pd.to_datetime(messages['timestamp_ms'], unit = 'ms').dt.strftime('%Y-%m-%d')
-    messages['month'] = pd.to_datetime(messages['timestamp_ms'], unit = 'ms').dt.strftime('%Y-%m')
-    messages['id'] = messages.index
+    messages_df = make_messages(d)
     # Participants
-    tmp = []
-    for participant in d['participants']:
-        tmp.append(participant)
-    participants = pd.DataFrame(tmp)
-    return messages, participants
+    participants_df = make_participants(d)
+    # Reactions
+    reactions = make_reactions(d)
+    return messages_df, participants_df
+
 
 if __name__ == '__main__':
     messages, participants = make_dataset("data/raw/sundayrose/message_1.json")
